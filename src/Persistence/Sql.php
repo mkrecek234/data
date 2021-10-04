pwFoo/data-1

test with forced fallback order

src/Persistence/Sql.php
@@ -339,6 +339,10 @@ class Sql extends Persistence
      */
     protected function setLimitOrder(Model $m, Query $q)
     {
+        foreach ($m->getFields() as $k => $f) {
+            $q->order($k, false);
+        }
+        
         // set limit
         if ($m->limit && ($m->limit[0] || $m->limit[1])) {
             if ($m->limit[0] === null) {

mkrecek234/data

src/Persistence/Sql.php
@@ -339,6 +339,10 @@ class Sql extends Persistence
      */
     protected function setLimitOrder(Model $model, Query $query): void
     {
+        foreach ($model->getFields() as $k => $f) {
+            $query->order($k, false);
+        }
+        
         // set limit
         if ($model->limit && ($model->limit[0] || $model->limit[1])) {
             if ($model->limit[0] === null) {
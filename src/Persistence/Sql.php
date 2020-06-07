8e73ec4ed3e9cafb360ada339c0b689ec3a9fa4a
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-08 00:05:22 +0200

    test with forced fallback order

--- src/Persistence/Sql.php
+++ src/Persistence/Sql.php
@@ -339,6 +339,10 @@ class Sql extends Persistence
      */
     protected function setLimitOrder(Model $model, Query $query): void
     {
+        foreach ($model->getFields() as $k => $f) {
+            $query->order($k, false);
+        }
+        
         // set limit
         if ($model->limit && ($model->limit[0] || $model->limit[1])) {
             if ($model->limit[0] === null) {
